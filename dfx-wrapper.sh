106b70bb3115ba2623e7a6dbbf8474960f791112
Scott Scoble <sscoble@scotts-laptop.lan>
2024-02-14 22:36:21 -0800

    Initial Commit - let's set this off

---
+++ dfx-wrapper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Determine the architecture of the machine
+ARCH="$(uname -m)"
+
+# Path to the dfx-wrapped binary
+DFX_BIN="$out/bin/dfx-wrapped"
+
+if [[ "$ARCH" == "arm64" ]]; then
+  # If we're on Apple Silicon, use Rosetta to run the dfx-wrapped binary
+  exec arch -x86_64 "$DFX_BIN" "$@"
+else
+  # Otherwise, run the dfx-wrapped binary directly
+  exec "$DFX_BIN" "$@"
+fi

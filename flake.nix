eecca7f180b8ec1a90f5ef29f177db7ee2f50ff7
Scott Scoble <wscoble@users.noreply.github.com>
2024-02-14 22:36:58 -0800

    Initial Commit - let's set this off (#1)
    Co-authored-by: Scott Scoble <sscoble@scotts-laptop.lan>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A flake for the DFINITY SDK (dfx) with Apple Silicon support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: let
+
+    version = "000";
+    shas = {
+      "x86_64-linux" = "000";
+      "x86_64-darwin" = "000";
+    };
+
+  in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        dfx-bin = pkgs.stdenv.mkDerivation {
+          name = "dfx-${version}";
+          src = pkgs.fetchurl {
+            url = "https://github.com/dfinity/sdk/releases/download/${version}/dfx-${version}-x86_64-darwin.tar.gz";
+            sha256 = shas."x86_64-darwin";
+          };
+          buildInputs = [ pkgs.makeWrapper ];
+          installPhase = ''
+            mkdir -p $out/bin
+            tar -xzf $src -C $out/bin
+            
+            # Rename the dfx binary to dfx-wrapped
+            mv $out/bin/dfx $out/bin/dfx-wrapped
+            
+            # Install the wrapper script as dfx
+            cp ${./dfx-wrapper.sh} $out/bin/dfx
+            chmod +x $out/bin/dfx
+            
+            # Use makeWrapper to adjust the wrapper, ensuring it calls dfx-wrapped
+            wrapProgram $out/bin/dfx --add-flags "$out/bin/dfx-wrapped"
+          '';
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ dfx-bin ];
+        };
+      });
+}

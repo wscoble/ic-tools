d4c510138469b3a1a87f0669208be685bfe252cf
wscoble <287393+wscoble@users.noreply.github.com>
2024-02-26 00:11:01 +0000

    [create-pull-request] automated change

--- flake.nix
+++ flake.nix
@@ -8,9 +8,9 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs: let
 
-    version = "0.17.0";
-    shas."x86_64-linux" = "15346005975c4183d5da6e3a4ea828e58b76d65c0e57bdbe0edbc42a4536a465";
-    shas."x86_64-darwin" = "9d1dd243cec8fa26fe3f4eec0d86950435cba6ca29d6afd09b86097fd5def318";
+    version = "null";
+    shas."x86_64-linux" = "Not";
+    shas."x86_64-darwin" = "Not";
 
   in
     flake-utils.lib.eachDefaultSystem (system:
